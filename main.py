89d2b2d45fff3eb847011abbbec02d9a73d5e622
sadric5 <sadricgamer@gmail.com>
2022-03-31 23:14:07 -0600

    update the main file

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
+import re
+
 def main(word):
-	print("Let's start our guessing Game :)\n")
+	print("Let's start our guessing Letter Game :)\n")
 	lengthWord = len(word)
 
 	placeHoldForWord = ['__' for i in word]
-	print(".".join(placeHoldForWord))
+
+	checker = True
+	cloneWord = list(word)
+	while checker:
+
+		userLetter = input("Enter Your Letter:")
+
+
+		if userLetter in cloneWord:
+			letterPosition = cloneWord.index(userLetter)
+			cloneWord[letterPosition] = "+"
+			placeHoldForWord[letterPosition] = userLetter
+		elif userLetter=="!" or "".join(placeHoldForWord)==word:
+			checker = False
+
+
+
+
+		print(".".join(placeHoldForWord))
 
 
 
