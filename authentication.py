5a151fe9a57b7e08cec949de4add6efd0a900624
sadric5 <sadricgamer@gmail.com>
2022-04-13 10:57:17 -0600

    implement the add new user function

--- authentication.py
+++ authentication.py
@@ -7,12 +7,12 @@ def authorizeAccess(username, password):
 
 def permissionsToAddNewUser(username, password, password2):
     # check for the password requirement before create user.
-    if len(password)>=6:
+    if (len(password)>=6 and password==password2) and len(username)>=2:
         createUser(username, password)
-        return "Account created successfully. Please login :)"
+        return (True, "Account created successfully. Please login :)")
     else:
         # it's just my basic requiement. Should be change in production.
-        return "Password does't requirement (7 characters, 1 capital letter, 2 digits). Try again :)"
+        return (False, "Password does't requirement (7 characters, 1 capital letter, 2 digits). Try again :)")
     
 
 if __name__ == '__main__':
